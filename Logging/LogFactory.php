13f969045964924dca75cc63ad17d62a287768bb
bartee <bart.stroeken@gmail.com>
2011-05-10 04:33:52 -0700

    Disabling of the logging caused a bug.

--- Logging/LogFactory.php
+++ Logging/LogFactory.php
@@ -21,19 +21,23 @@ class LogFactory {
 	 * @return BaseLogger
 	 */
 	public static function get($name) {
-		if (defined('LOGGING_ACTIVE') && LOGGING_ACTIVE == false){
-			/**
-			 * Logging has been disabled, so do nothing
-			 */
-			return false;
-		}
-		
 		if (!is_array(self::$_loggers)){
 			self::$_loggers = array();
 			
 			include_once LOGGING_BASEDIR.'/Logging/loggers/BaseLogger.php';
+			include_once LOGGING_BASEDIR.'/Logging/loggers/DummyLogger.php';
+
 			self::$_loggers['BaseLogger'] = new BaseLogger();
+			self::$_loggers['DummyLogger'] = new DummyLogger();
 		}
+
+		if (defined('LOGGING_ACTIVE') && LOGGING_ACTIVE == false){
+			/**
+			 * Logging has been disabled, so return a dummy logger
+			 */
+			return self::$_loggers['DummyLogger'];
+		}
+		
 		if (!key_exists($name, self::$_loggers)){
 			try { 
 
